0baf16627b44ea6852d5b78e960777b81f5e039d
accaciac <accacia.charles@gmail.com>
2021-08-24 15:03:02 -0400

    menu styling

--- theRestaurantMall/java.js
+++ theRestaurantMall/java.js
@@ -1,61 +1,22 @@
-//Pseudo Code
 
-//1 GOAL: When user hovers on mobile menu the menu icon the menu appears
-
-    //Grab the button with the "mobile-menu" class
     const mobileMenu = document.querySelector('.mobile-menu');
-    
-    //Grab the ul with class "nav-link-flex"
     const subMenu = document.querySelector('.nav-link-flex');
-    
-    //Grab the a element
     const navLinks = document.querySelectorAll('ul li a');
-    console.log(navLinks);
 
-    //add an event listener to listen to when user hovers over the button with the "mobile-menu" class.
     mobileMenu.addEventListener('mouseover', function(){
-            //display the menu
             subMenu.classList.toggle('hidden-menu');
-            console.log("Im mousing over")
-    });
-    //add an event listener to listen to when user hovers out of the navigation.
-    subMenu.addEventListener('mouseleave', function(){
-            //menu disappears
-            subMenu.classList.add('hidden-menu');
-            console.log("Im mouseleave");            
     });
 
-    //add an event listener to listen to when user clinks on one of the nav links the menu disappears
+    subMenu.addEventListener('mouseleave', function(){
+            subMenu.classList.add('hidden-menu');           
+    });
 
     for (let i = 0; i < navLinks.length; i = i + 1){
         navLinks[i].addEventListener('click' , function(){
-                //menu disappears
-                subMenu.classList.add('hidden-menu');
-                console.log("Im hidding!")                
+                subMenu.classList.add('hidden-menu');              
         })
     }
-//---------------------------------
 
-// 2 GOAL: When page loads the "header image" on all the pages will do an animation (slide from top of page to its position)
-
-    //Grab the header image
-
-    
-    //add an event listener to listen to when page loads/reloads to do the animation
-
-    // add the animation css styling to scss sheet.
-
-    // DOMContentLoaded is when the page is loading.
-    add.addEventListener('DOMContentLoaded', function(){
-            
-    })
-
-        //add a class to "header image"
-
-//-----------------------------------
-
-// 3 GOAL: When the user scrolls down the page a prompt screen that has the contact info and ask if you have any inquiries appears. 
-//
 
 
 
